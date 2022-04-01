6d238ba2fabe44ca6656573be9a8cbc01f902e0f
efarias03 <89557986+efarias03@users.noreply.github.com>
2022-04-01 19:12:24 -0300

    Add files via upload

---
+++ main/main.js
@@ -0,0 +1,7 @@
+document.querySelector('.top-nfts-cards').addEventListener('wheel', event =>{
+    if (event.deltaY > 0) {
+        event.target.scrollBy (-450 , 0)
+    } else (
+        event.target.scrollBy (450 , 0)
+    )
+    event.preventDefault();})
