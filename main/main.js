7e7d052155af64cccba158e2c0899f37832672e2
efarias03 <89557986+efarias03@users.noreply.github.com>
2022-04-02 19:25:57 -0300

    Update main.js

--- main/main.js
+++ main/main.js
@@ -5,3 +5,11 @@ document.querySelector('.top-nfts-cards').addEventListener('wheel', event =>{
         event.target.scrollBy (450 , 0)
     )
     event.preventDefault();})
+
+document.querySelector('.nft-creators').addEventListener('wheel', event =>{
+    if (event.deltaY > 0) {
+        event.target.scrollBy (-450 , 0)
+    } else (
+        event.target.scrollBy (450 , 0)
+    )
+    event.preventDefault();})
